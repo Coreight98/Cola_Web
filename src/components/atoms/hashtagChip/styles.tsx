15f1d8baf412a4a107bcf63f88576fd5c1447fe0
dmstmdrbs <dmstmdrbs98@gmail.com>
2022-03-10 20:59:16 +0900

    style: modify hashtag chip style

--- src/components/atoms/hashtagChip/styles.tsx
+++ src/components/atoms/hashtagChip/styles.tsx
@@ -8,28 +8,26 @@ const Container = styled.div<HashtagChipInterface>`
   justify-content: space-between;
   align-items: center;
   height: 30px;
-  width: ${(props) => (props.size === 'full' ? 70 : 30)}px;
+  width: 100%;
   border-radius: 15px;
   background-color: #4b48e7;
   opacity: 0.9;
   transition: 0.1s;
   padding: 0 5px 0 10px;
+  margin: 0.2rem 0;
   color: whitesmoke;
   cursor: pointer;
   &:hover {
     background-color: #5f5cec;
   }
-  & > button {
-    display: ${(props) => (props.size === 'full' ? 'block' : 'none')};
-  }
 `;
 const RemoveBtn = styled.button`
-  /* border: 0; */
   background: none;
   border-radius: 100%;
   transition: 0.1s;
   color: whitesmoke;
   cursor: pointer;
+  border: none;
   &:hover {
     background-color: #282f91;
     opacity: 0.8;
