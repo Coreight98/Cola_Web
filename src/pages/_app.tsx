e48f9c6c7b34bc3e1598813d0a8be125dd5e7f55
coreight98 <maxcha98@naver.com>
2022-03-15 21:35:14 +0900

    feat : add themeProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,20 @@
 import '@styles/globals.css';
+import { ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 
 import Layout from '@components/Layout';
+import { theme } from '@styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <Layout>
-        <Component {...pageProps}></Component>
-      </Layout>
-    </RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <RecoilRoot>
+        <Layout>
+          <Component {...pageProps}></Component>
+        </Layout>
+      </RecoilRoot>
+    </ThemeProvider>
   );
 }
 
