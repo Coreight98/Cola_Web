d01b5e30c32de4b02a9e571c7781f32f9a4c468b
coreight98 <maxcha98@naver.com>
2022-04-01 17:24:42 +0900

    feat : signUp page and refactor unnecessary logic 디자인에 따른 회원가입 UI를 수정하였습니다. 불필요한 로직을 줄였습니다.

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,10 +1,13 @@
 import { Theme } from '@emotion/react';
+import { string } from 'yup';
 declare module '@emotion/react' {
   export interface Theme {
     colors: {
       White: string;
       Begonia: string;
       Emerald: string;
+      VeryLightBlue: string;
+      JordyBlue: string;
     };
   }
 }
@@ -13,5 +16,7 @@ export const theme: Theme = {
     White: '#FFFFFF',
     Begonia: '#FF7373',
     Emerald: '#5ED68E',
+    VeryLightBlue: '#6C7BFA',
+    JordyBlue: '#90A8F0',
   },
 };
