e2d3f0d84d1b90efc1901d9d8c7f78b7190faffb
coreight98 <maxcha98@naver.com>
2022-04-01 17:19:58 +0900

    refactor : fix button component and add auth & submit button 기존 버튼의 태그를 buttonstyle로 변경하였습니다. 재사용성을 위해 디폴트 버튼에서 스타일을 수정한 auth & submit버튼을 생성하여 사용하였습니다.

---
+++ src/components/atoms/button/styles.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const ButtonStyle = styled.button`
+  border: none;
+  :hover {
+    cursor: pointer;
+    opacity: 0.5;
+  }
+`;
+
+export { ButtonStyle };
