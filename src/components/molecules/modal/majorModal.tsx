2599e337c9be03619ff4eec7703857ae4fe03e61
coreight98 <maxcha98@naver.com>
2022-04-02 15:27:06 +0900

    refacor : change to entries and dividee to props

--- src/components/molecules/modal/majorModal.tsx
+++ src/components/molecules/modal/majorModal.tsx
@@ -5,25 +5,22 @@ import { Dispatch, SetStateAction, useState } from 'react';
 import { TypeOf } from 'yup';
 import { theme } from '@styles/theme';
 
-const MajorModal = ({
-  major,
-  setMajor,
-  setModalOnOff,
-}: {
+interface Props {
   major: keyof typeof MAJOR_TYPE;
   setMajor: Dispatch<SetStateAction<keyof typeof MAJOR_TYPE>>;
   setModalOnOff: Dispatch<SetStateAction<boolean>>;
-}) => {
+}
+
+const MajorModal = ({ major, setMajor, setModalOnOff }: Props) => {
   const [tempMajor, setTempMajor] = useState<keyof typeof MAJOR_TYPE>(major);
-  const handleClick = () => {};
   return (
     <>
       <Title>학과</Title>
       <MajorWrapper>
-        {Object.keys(MAJOR_TYPE).map((element) => (
-          <label onClick={() => setTempMajor(element as keyof typeof MAJOR_TYPE)}>
-            <CheckBox selected={tempMajor === element} />
-            <Span selected={tempMajor === element}>{MAJOR_TYPE[element as keyof typeof MAJOR_TYPE]}</Span>
+        {Object.entries(MAJOR_TYPE).map(([key, value]) => (
+          <label onClick={() => setTempMajor(key as keyof typeof MAJOR_TYPE)}>
+            <CheckBox selected={tempMajor === key} />
+            <Span selected={tempMajor === key}>{value}</Span>
           </label>
         ))}
       </MajorWrapper>
