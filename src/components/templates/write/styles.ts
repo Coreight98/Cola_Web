f0bc89779d960aef84e7796b33458b347c827c09
coreight98 <maxcha98@naver.com>
2022-04-12 21:16:37 +0900

    refactor : add icon and css style ( using grid )

--- src/components/templates/write/styles.ts
+++ src/components/templates/write/styles.ts
@@ -1,18 +1,70 @@
 import styled from '@emotion/styled';
+import { theme } from '@styles/theme';
 
 const Container = styled.div`
-  width: 80vw;
-  height: 100%;
-  padding: 1rem 2rem;
+  padding: 5rem 0px;
+  display: grid;
+  column-gap: 1rem;
+  row-gap: 1rem;
+  grid-template:
+    'title . mode .' 1fr
+    'contents contents contents hashtagBar' 10fr
+    '. . btn .' 1fr
+    / 6fr 4fr 2fr 2fr;
+  @media (max-width: ${theme.breakpoints.md}) {
+    grid-template:
+      'title . mode .' 1fr
+      'contents contents contents .' 10fr
+      'hashtagBar hashtagBar btn .' 1fr
+      / 6fr 4fr 2fr 2fr;
+  }
+  @media (max-width: ${theme.breakpoints.sm}) {
+    grid-template:
+      'title . mode' 1fr
+      'contents contents contents' 10fr
+      'hashtagBar hashtagBar btn' 1fr
+      / 6fr 4fr 2fr;
+  }
 `;
 
-const EditorWrapper = styled.div`
+const HashtagBar = styled.div`
+  grid-area: hashtagBar;
+  border-radius: 30px;
+  padding: 2rem;
+  max-width: 10vw;
+  background: ${theme.colors.Vodka};
   display: flex;
-  align-items: center;
+  overflow: auto;
+  overflow-x: hidden;
+  flex-direction: column;
+  align-items: flex-start;
+  box-shadow: inset 0px 0px 6px #00000029;
+  @media (max-width: ${theme.breakpoints.md}) {
+    flex-direction: row;
+    overflow-x: auto;
+    overflow-y: hidden;
+    place-items: center;
+    max-width: 100%;
+  }
 `;
+
 const Wrapper = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: space-evenly;
+  align-items: center;
+  box-shadow: 0px 0px 6px #00000029;
+  border-radius: 15px;
 `;
 
-export { Container, EditorWrapper, Wrapper };
+const TitleInput = styled.input`
+  grid-area: title;
+  display: flex;
+  justify-content: flex-start;
+  border: none;
+  background: none;
+  ::placeholder {
+    color: ${theme.colors.VeryLightBlue};
+  }
+`;
+
+export { Container, HashtagBar, Wrapper, TitleInput };
