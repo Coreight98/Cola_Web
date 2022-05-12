2db9b4903ebe91f54f314aed7cc74f29b8efcc1b
coreight98 <maxcha98@naver.com>
2022-05-12 14:44:15 +0900

    feat : add socket lib logic

--- src/utils/libs/useChat.tsx
+++ src/utils/libs/useChat.tsx
@@ -1,23 +1,25 @@
-import { useState, useEffect, Dispatch, SetStateAction } from 'react';
+import { useState, useEffect, Dispatch, SetStateAction, useRef, MutableRefObject } from 'react';
 
 import { Socket } from 'socket.io-client';
 
 import useInput, { UseInputProps } from './useInput';
 
-import { IUser, IChat, IUsers } from '~/types/gather';
+import { IUser, IChat } from '~/types/gather';
 
 type UseChatProps = [
   ...UseInputProps,
   (e: React.KeyboardEvent<HTMLInputElement>) => void,
   IChat[],
   Dispatch<SetStateAction<IChat[]>>,
+  MutableRefObject<HTMLSelectElement | undefined>,
 ];
 export default function useChat(socket: Socket, user: IUser): UseChatProps {
+  const selectRef = useRef<HTMLSelectElement>();
   const [chatInput, setChatInput, onChangeChatInput] = useInput('');
   const [chatList, setChatList] = useState<IChat[]>([]);
 
   useEffect(() => {
-    socket.on('receive-chat', (chat: IChat) => {
+    socket.on('RECEIVE_MESSAGE', (chat: IChat) => {
       setChatList((prev) => [...prev, chat]);
     });
   }, []);
@@ -25,18 +27,20 @@ export default function useChat(socket: Socket, user: IUser): UseChatProps {
   const onKeyPressEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       e.preventDefault();
+
       const newChat: IChat = {
+        status: selectRef.current?.value as string,
         text: chatInput,
         name: user.name,
         cId: user.cId,
         date: Date.now(),
-        id: Math.random().toString(32).substring(2, 11),
+        id: socket.id,
       };
-      socket.emit('send-chat', newChat);
+      socket.emit('SEND_MESSAGE', newChat);
       setChatList((prev) => [...prev, newChat]);
       setChatInput('');
     }
   };
 
-  return [chatInput, setChatInput, onChangeChatInput, onKeyPressEnter, chatList, setChatList];
+  return [chatInput, setChatInput, onChangeChatInput, onKeyPressEnter, chatList, setChatList, selectRef];
 }
