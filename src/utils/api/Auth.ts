2f1b15bdde7e5f6835704b4755de1ed851c5974d
TomEun <tom.eun@devercorp.com>
2022-05-15 23:43:35 +0900

    feat: add Auth api module & sig nup api
    계정 관련 API를 위한 Auth 모듈 추가
    email 체크 및 인증  체크 api 연동

--- src/utils/api/Auth.ts
+++ src/utils/api/Auth.ts
@@ -1,37 +1,13 @@
 import Api from './core';
 
-const responseBody = (message: string) => {
-  return {
-    code: 200,
-    status: 'success',
-    data: {
-      message: message,
-    },
-  };
-};
-const promiseResponse = (message: string) =>
-  new Promise((resolve) =>
-    setTimeout(() => {
-      resolve(responseBody(message));
-    }, 1000),
-  );
-
 const Auth = {
-  policyAgreement({ service, privacy }: any) {
-    console.log('policyAgreement', service, privacy);
-    if (service && privacy) return promiseResponse('이용약관 동의 성공');
-    else
-      return new Promise((resolve) =>
-        resolve({ code: 401, status: 'fail', data: { message: '이용약관에 모두 동의해주세요.' } }),
-      );
+  async checkEmail(email: string) {
+    const { data } = await Api.post('/api/v1/auth/mail', { email });
+    return data;
   },
-  checkEmail(email: string) {
-    console.log('checkEmail', email);
-    return promiseResponse('이메일 인증 성공');
-  },
-  signup(payload: any) {
-    console.log('signup', payload);
-    return promiseResponse('회원가입 성공');
+  async checkEmailCode(token: string) {
+    const { data } = await Api.post('/api/v1/auth/confirm', { token });
+    return data;
   },
 };
 export default Auth;
